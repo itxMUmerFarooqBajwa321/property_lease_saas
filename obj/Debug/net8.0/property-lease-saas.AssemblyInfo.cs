27572efa531604fd12c50942719fb1986eb3d85f
itxMUmerFarooqBajwa321 <umerfarooqbajwa321@gmail.com>
2026-03-28 06:25:31 +0500

    created new build

--- obj/Debug/net8.0/property-lease-saas.AssemblyInfo.cs
+++ obj/Debug/net8.0/property-lease-saas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("property-lease-saas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a54fbb7fcf71792dccebc022797fbae84be7659f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0264d9fec8d367d8568a18d0899330a971be599")]
 [assembly: System.Reflection.AssemblyProductAttribute("property-lease-saas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("property-lease-saas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
